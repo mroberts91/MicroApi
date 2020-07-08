d57cc96158f02908f30671d6cb141738284c3c3d
mrobertson <mrobertson@SFDEV050>
2020-07-08 15:13:49 -0400

    Modified code layout

--- MicroApi/Program.cs
+++ MicroApi/Program.cs
@@ -2,27 +2,68 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MicroApi;
+using Microsoft.AspNetCore.Http.Json;
+using MicroApi.Data;
+using System.Net;
 
-namespace MicroApi
-{
-    public class Program
+var Repo = new TodoRepository();
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
     {
-        public static void Main(string[] args)
+        webBuilder.Configure(app =>
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
                 {
-                    webBuilder.Configure(app =>
-                    {
-                        app.UseDeveloperExceptionPage();
-                        app.UseHttpsRedirection();
-                        app.UseRouting();
-                        app.DeclareTodoEndpoints();
-                    });
+                    await context.Response.WriteAsJsonAsync("Micro CRUD API");
                 });
-    }
-}
+
+                endpoints.MapGet("/todos", async context =>
+                {
+                    var lists = await Repo.GetTodoListsAsync();
+                    context.Response.StatusCode = lists is null ? (int)HttpStatusCode.NotFound : (int)HttpStatusCode.OK;
+                    await context.Response.WriteAsJsonAsync(lists);
+                });
+
+                endpoints.MapGet("/todos/{id:int}", async context =>
+                {
+                    var id = context.Request.GetListId();
+                    var list = await Repo.GetTodoListAsync(id);
+
+                    context.Response.StatusCode = list is null ? (int)HttpStatusCode.NotFound : (int)HttpStatusCode.OK;
+                    await context.Response.WriteAsJsonAsync(list);
+                });
+
+                endpoints.MapPost("/todos", async context =>
+                {
+                    var result = await Repo.CreateTodoListAsync(await context.Request.GetTodoRequestBody());
+
+                    context.Response.StatusCode = (int)HttpStatusCode.Created;
+                    await context.Response.WriteAsJsonAsync(result);
+                });
+
+                endpoints.MapPut("/todos", async context =>
+                {
+                    var result = await Repo.UpdateTodoListAsync(await context.Request.GetTodoRequestBody());
+
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    await context.Response.WriteAsJsonAsync(result);
+                });
+
+                endpoints.MapDelete("/todos/{id:int}", async context =>
+                {
+                    var id = context.Request.GetListId();
+                    await Repo.DeleteTodoListAsync(id);
+
+                    context.Response.StatusCode = (int)HttpStatusCode.NoContent;
+                });
+            });
+        });
+    })
+    .Build()
+    .Run();
---
+++ MicroApi/TodoRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MicroApi.Data;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MicroApi
+{
+    public static class TodoRequestExtensions
+    {
+        public static long GetListId(this HttpRequest req)
+        {
+            if (!long.TryParse((string)req.RouteValues["id"], out var id))
+                throw new InvalidCastException("Parameter id was not present in the request.");
+            return id;
+        }
+
+        public static async Task<TodoList> GetTodoRequestBody(this HttpRequest request)
+        {
+            using var reader = new StreamReader(request.Body, Encoding.UTF8);
+            return JsonSerializer.Deserialize<TodoList>(await reader.ReadToEndAsync())
+                ?? throw new NullReferenceException($"{nameof(TodoList)} object was not present in the request body.");
+        }
+    }
+}
